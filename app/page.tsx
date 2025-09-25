8c23fc7df804a395b98c9156c35a09c46a5864a5
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-25 18:07:57 +0900

    implement all the navbar elements for md and above;

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <h1>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle />
-    </h1>
-  );
+  return <h1>Home Page Content</h1>;
 }
