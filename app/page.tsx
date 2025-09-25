52abc1dbfb66ed10f4ae753ac1c45db3c87b3146
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-25 17:39:15 +0900

    Add Clerk authentication and update app metadata
    - Integrated ClerkProvider in layout for authentication management.
    - Updated metadata for the application title and description.
    - Implemented sign-in and user button functionality in the home page.
    - Added middleware for Clerk authentication handling.
    - Updated package.json and package-lock.json to include @clerk/nextjs dependency.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <h1>Hello Home page</h1>;
+  return (
+    <h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-red-500">
+            <span>Sign In</span>
+          </button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </h1>
+  );
 }
